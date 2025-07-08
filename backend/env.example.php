21516bcb1552f0ad60e7115746b87144613b78aa
razherana <razherana@gmail.com>
2025-07-08 09:02:22 +0300

    modified env example

--- backend/env.example.php
+++ backend/env.example.php
@@ -1,6 +1,7 @@
 <?php
 
-define('USER', 'razherana');
+define('USER', 'root');
 define('PASSWORD', '');
 define('HOST', 'localhost');
 define('DB_NAME', 's4_exam_web');
+define('JWT_SECRET', 'razherana_test_cool_herana_herana_3216_herana_herana');
